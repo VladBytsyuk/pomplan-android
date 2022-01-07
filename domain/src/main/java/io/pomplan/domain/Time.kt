1f417d28bea0530e2284a44b82d84ea6eb3f1b81
Vladislav Bytsyuk <vladislav.bytsyuk@gmail.com>
2022-01-08 02:06:48 +0300

    Add construction check on `Time` class

--- domain/src/main/java/io/pomplan/domain/Time.kt
+++ domain/src/main/java/io/pomplan/domain/Time.kt
@@ -5,8 +5,19 @@ value class Time(private val millis: Long = 0L) : Comparable<Time> {
     constructor(minutes: Int = 0, seconds: Int = 0, milliseconds: Int = 0) :
         this(millis = minutes.minutes + seconds.seconds + milliseconds.milliseconds)
 
-    operator fun plus(other: Time) = Time(millis = this.millis + other.millis)
-    operator fun minus(other: Time) = Time(millis = maxOf(0, this.millis - other.millis))
+    init {
+        require(millis >= 0L) { "Time can't have negative millis." }
+    }
+
+    operator fun plus(other: Time) = Time(this.millis + other.millis)
+    operator fun minus(other: Time) = Time(maxOf(0, this.millis - other.millis))
+
+    override fun toString(): String {
+        val minutes = millis / MILLIS_IN_MINUTE
+        val seconds = millis % MILLIS_IN_MINUTE / MILLIS_IN_SECOND
+        val millis = millis % MILLIS_IN_SECOND
+        return "$minutes:$seconds.$millis"
+    }
 
     override fun compareTo(other: Time) = this.millis.compareTo(other.millis)
 }
