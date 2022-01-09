79c11e7b935b1901c424f0ec970c87e554ecc156
Vladislav Bytsyuk <vladislav.bytsyuk@gmail.com>
2022-01-09 13:22:34 +0300

    Add `+` operator overriding  for `Pomodoro` class

--- domain/src/main/java/io/pomplan/domain/Pomodoro.kt
+++ domain/src/main/java/io/pomplan/domain/Pomodoro.kt
@@ -3,14 +3,14 @@ package io.pomplan.domain
 class Pomodoro(
     val type: Type,
     val fullTime: Time,
-    val elapsedTime: Time,
+    val elapsedTime: Time = Time(millis = 0L),
 ) {
     enum class Type { WORK, SHORT_BREAK, LONG_BREAK }
 
     val restTime: Time get() = fullTime - elapsedTime
     val isCompleted: Boolean get() = restTime.isEmpty
 
-    fun addElapsedTime(time: Time) = Pomodoro(
+    operator fun plus(time: Time) = Pomodoro(
         type = this.type,
         fullTime = this.fullTime,
         elapsedTime = this.elapsedTime + time
